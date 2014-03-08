9a2333dbb014ff693cc047ab0a4e900cdbf13e98
rekarnar <rekarnar@gmail.com>
2014-03-08 23:59:39 +0530

    adding backbone picky lib

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -7,6 +7,7 @@ requirejs.config({
         dust                   : '../bower_components/dustjs-linkedin/lib/dust',
         dustHelpers            : '../bower_components/dustjs-linkedin-helpers/lib/dust-helpers',
         dustMarionette         : '../bower_components/marionette-dust/src/backbone.marionette.dust',
+        'backbone.picky'       : '../bower_components/backbone.picky/lib/amd/backbone.picky',
         'backbone.wreqr'       : '../bower_components/backbone.wreqr/lib/amd/backbone.wreqr',
         'backbone.eventbinder' : '../bower_components/backbone.eventbinder/lib/amd/backbone.eventbinder',
         'backbone.babysitter'  : '../bower_components/backbone.babysitter/lib/amd/backbone.babysitter',
